0baf506cbeea2bcd255afa7856d07d941a43274c
Robert Stoll <rstoll@tutteli.ch>
2014-12-06 13:27:04 +0100

    - TINS-244 scope is not set on catch type -- regression test

---
+++ test/ch/tsphp/tinsphp/inference_engine/test/integration/definition/CatchTest.java
@@ -0,0 +1,63 @@
+/*
+ * This file is part of the TinsPHP project published under the Apache License 2.0
+ * For the full copyright and license information, please have a look at LICENSE in the
+ * root folder or visit the project's website http://tsphp.ch/wiki/display/TINS/License
+ */
+
+package ch.tsphp.tinsphp.inference_engine.test.integration.definition;
+
+import ch.tsphp.tinsphp.inference_engine.test.integration.testutils.ScopeTestHelper;
+import ch.tsphp.tinsphp.inference_engine.test.integration.testutils.ScopeTestStruct;
+import ch.tsphp.tinsphp.inference_engine.test.integration.testutils.definition.ADefinitionScopeTest;
+import org.antlr.runtime.RecognitionException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+@RunWith(Parameterized.class)
+public class CatchTest extends ADefinitionScopeTest
+{
+
+    public CatchTest(String testString, ScopeTestStruct[] theTestStructs) {
+        super(testString, theTestStructs);
+    }
+
+    @Test
+    public void test() throws RecognitionException {
+        check();
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> testStrings() {
+        List<Object[]> collection = new ArrayList<>();
+
+        collection.addAll(getVariations("", "", "\\.\\.", new Integer[]{1}));
+        collection.addAll(getVariations("namespace{", "}", "\\.\\.", new Integer[]{1}));
+
+        //nBody function block
+        collection.addAll(getVariations("function foo(){", "}",
+                "\\.\\.foo().", new Integer[]{1, 0, 4}));
+
+        //TODO rstoll TINS-161 inference OOP
+        //nBody class classBody mDecl block
+//        collection.addAll(getVariations("class a{ function void foo(){", "}}",
+//                "\\.\\.a.foo().", new Integer[]{1, 0, 4, 0, 4}));
+        return collection;
+    }
+
+    public static Collection<Object[]> getVariations(String prefix, String appendix,
+            String fullScopeName, Integer[] accessToScope) {
+        Integer[] stepIn = new Integer[]{};
+        return Arrays.asList(new Object[][]{
+                {prefix + "try{}catch(Exception $e){};" + appendix, new ScopeTestStruct[]{
+                        new ScopeTestStruct("Exception", fullScopeName,
+                                ScopeTestHelper.getAstAccessOrder(accessToScope, stepIn, 0, 1, 0)),
+                }},
+        });
+    }
+}
